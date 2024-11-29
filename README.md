18781c2b8b92b6cb862e06c99a468aaa8d02da9c
lxf <infiniment@163.com>
2024-11-29 13:42:38 +0800

    feat: add optional parameter  `returnType`

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Reads the contents of the directory synchronously and recursively.
 
 Like `fs.readdirSync('/path/source', { recursive: true })`.Compatible with lower versions of nodejs.
 
+Return `[]` if the path not exist or not a directory
+
 ## Installation
 
 ```js
@@ -15,4 +17,8 @@ npm install @lxf2513/readdir-sync-recursive
 import readdirSyncRecursive from '@lxf2513/readdir-sync-recursive'
 
 readdirSyncRecursive('/path/source')
+
+readdirSyncRecursive('/path/source', 'relativePath')
+
+readdirSyncRecursive('/path/source', 'absolutePath')
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,39 @@
 import fs from 'node:fs';
 import nodePath from 'node:path';
+import process from 'node:process';
 
 function isDirectory(path: string) {
   try {
     const stat = fs.statSync(path);
     return stat.isDirectory();
-  } catch (error) {
+  } catch {
     return false;
   }
 }
 
-export default function (basePath: string) {
+export default function (
+  path: string,
+  returnType?: 'absolutePath' | 'relativePath'
+) {
+  if (!isDirectory(path)) {
+    return [];
+  }
   const readdirResults: string[] = [];
-  const pathsQueue = [basePath];
-  function readdir(path: string) {
+  const pathsQueue = [path];
+  function readdir(dirpath: string) {
     try {
-      const readdirResult = fs.readdirSync(path, { encoding: 'utf-8' });
+      const readdirResult = fs.readdirSync(dirpath, { encoding: 'utf-8' });
       for (let i = 0; i < readdirResult.length; i++) {
-        const resultPath = nodePath.join(path, readdirResult[i]);
-        const relativeResultPath = nodePath.relative(basePath, resultPath);
-        readdirResults.push(relativeResultPath);
+        const resultPath = nodePath.join(dirpath, readdirResult[i]);
+        const relativeResultPath = nodePath.relative(path, resultPath);
+        const absolutePath = nodePath.join(process.cwd(), resultPath);
+        let pushPath = relativeResultPath;
+        returnType === 'relativePath'
+          ? (pushPath = resultPath)
+          : returnType === 'absolutePath'
+            ? (pushPath = absolutePath)
+            : relativeResultPath;
+        readdirResults.push(pushPath);
         if (isDirectory(resultPath)) {
           pathsQueue.push(resultPath);
         }
